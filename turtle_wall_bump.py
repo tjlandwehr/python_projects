24d86464a0f2321094779f79b036f793cecb0269
tjlandwehr <tjlandwehr@gmail.com>
2017-10-02 20:36:53 -0500

    This commit confines the turtles within the screen's bounds and bounces them back after bumping into the wall.
    Further additions could include:
    --increasing the turtle's sizes so they are more likely to bump into and bounce off each other.
    --finding the corresponding angle of bouncing so the turtle bounces at an equivalent degree to its initial bounce into the wall and/or other turtles

--- turtle_wall_bump.py
+++ turtle_wall_bump.py
@@ -1,5 +1,6 @@
-import random
 import turtle
+import random
+import math
 
 
 def which_quadrant(turtle):
@@ -26,11 +27,7 @@ def which_direction(turtle):
         return "q4"
 
 
-def find_distance(turtle, quadrant, angle_direction):
-    return True
-
-
-def move_distance(screen, turtle):
+def corner_distance(turtle, screen, quadrant, heading):
     left_bound = -screen.window_width() / 2
     right_bound = screen.window_width() / 2
     upper_bound = screen.window_height() / 2
@@ -45,42 +42,257 @@ def move_distance(screen, turtle):
     y_up_remaining = abs(upper_bound - turtle_y)
     y_down_remaining = abs(lower_bound - turtle_y)
 
-    left_hypotenuse = x_left_remaining / math.cos(turtle_angle)
-    right_hypotenuse = x_right_remaining / math.cos(turtle_angle)
-    up_adjacent = y_up_remaining / math.tan(turtle_angle)
-    down_adjacent = y_down_remaining / math.tan(turtle_angle)
-    left_opposite = math.tan(turtle_angle) * x_left_remaining
-    right_opposite = math.tan(turtle_angle) * x_right_remaining
+    if quadrant == "q1":
+        if x_right_remaining < 50 and y_up_remaining >= 50:
+            distance = (x_right_remaining / math.cos(math.radians(turtle_angle))) - 0.01
+            if distance < 50:
+                return distance
+            else:
+                return 50
+        elif x_right_remaining >= 50 and y_up_remaining < 50:
+            distance = (y_up_remaining / math.sin(math.radians(turtle_angle))) - 0.01
+            if distance < 50:
+                return distance
+            else:
+                return 50
+        elif x_right_remaining < 50 and y_up_remaining < 50:
+            x_hypotenuse = x_right_remaining / math.cos(math.radians(turtle_angle))
+            y_hypotenuse = y_up_remaining / math.sin(math.radians(turtle_angle))
+            x_add = math.cos(math.radians(turtle_angle)) * y_hypotenuse
+            y_add = math.sin(math.radians(turtle_angle)) * x_hypotenuse
+            if turtle_x + x_add <= right_bound and turtle_y + y_add <= upper_bound:
+                if x_hypotenuse > 50:
+                    return 50
+                else:
+                    return x_hypotenuse - 0.01
+            elif turtle_x + x_add > right_bound:
+                x_diff = x_add - ((turtle_x + x_add) - right_bound)
+                hypotenuse = x_diff / math.cos(math.radians(turtle_angle))
+                return hypotenuse - 0.01
+            elif turtle_y + y_add > upper_bound:
+                y_diff = y_add - ((turtle_y + y_add) - upper_bound)
+                hypotenuse = y_diff / math.sin(math.radians(turtle_angle))
+                return hypotenuse - 0.01
+        else:
+            return 50
+    elif quadrant == "q2":
+        if x_left_remaining < 50 and y_up_remaining >= 50:
+            distance = (x_left_remaining / math.cos(math.radians(abs(turtle_angle - 180)))) - 0.01
+            if distance < 50:
+                return distance
+            else:
+                return 50
+        elif x_left_remaining >= 50 and y_up_remaining < 50:
+            distance = (y_up_remaining / math.sin(math.radians(abs(turtle_angle - 180)))) - 0.01
+            if distance < 50:
+                return distance
+            else:
+                return 50
+        elif x_left_remaining < 50 and y_up_remaining < 50:
+            x_hypotenuse = x_left_remaining / math.cos(math.radians(abs(turtle_angle - 180)))
+            y_hypotenuse = y_up_remaining / math.sin(math.radians(abs(turtle_angle - 180)))
+            x_add = math.cos(math.radians(abs(turtle_angle - 180))) * y_hypotenuse
+            y_add = math.sin(math.radians(abs(turtle_angle - 180))) * x_hypotenuse
+            if turtle_x - x_add >= left_bound and turtle_y + y_add <= upper_bound:
+                if x_hypotenuse > 50:
+                    return 50
+                else:
+                    return x_hypotenuse - 0.01
+            elif turtle_x - x_add < left_bound:
+                x_diff = x_add - (abs((turtle_x - x_add) - left_bound))
+                hypotenuse = x_diff / math.cos(math.radians(abs(turtle_angle - 180)))
+                return hypotenuse - 0.01
+            elif turtle_y + y_add > upper_bound:
+                y_diff = y_add - ((turtle_y + y_add) - upper_bound)
+                hypotenuse = y_diff / math.sin(math.radians(abs(turtle_angle - 180)))
+                return hypotenuse - 0.01
+        else:
+            return 50
+    elif quadrant == "q3":
+        if x_left_remaining < 50 and y_down_remaining >= 50:
+            distance = (x_left_remaining / math.cos(math.radians(turtle_angle - 180))) - 0.01
+            if distance < 50:
+                return distance
+            else:
+                return 50
+        elif x_left_remaining >= 50 and y_down_remaining < 50:
+            distance = (y_down_remaining / math.sin(math.radians(turtle_angle - 180))) - 0.01
+            if distance < 50:
+                return distance
+            else:
+                return 50
+        elif x_left_remaining < 50 and y_down_remaining < 50:
+            x_hypotenuse = x_left_remaining / math.cos(math.radians(turtle_angle - 180))
+            y_hypotenuse = y_down_remaining / math.sin(math.radians(turtle_angle - 180))
+            x_add = math.cos(math.radians(turtle_angle - 180)) * y_hypotenuse
+            y_add = math.sin(math.radians(turtle_angle - 180)) * x_hypotenuse
+            if turtle_x - x_add >= left_bound and turtle_y - y_add >= lower_bound:
+                if x_hypotenuse > 50:
+                    return 50
+                else:
+                    return x_hypotenuse - 0.01
+            elif turtle_x - x_add < left_bound:
+                x_diff = x_add - (abs((turtle_x - x_add) - left_bound))
+                hypotenuse = x_diff / math.cos(math.radians(turtle_angle - 180))
+                return hypotenuse - 0.01
+            elif turtle_y - y_add < lower_bound:
+                y_diff = y_add - (abs((turtle_y - y_add) - lower_bound))
+                hypotenuse = y_diff / math.sin(math.radians(turtle_angle - 180))
+                return hypotenuse - 0.01
+        else:
+            return 50
+    else:
+        if x_right_remaining < 50 and y_down_remaining >= 50:
+            distance = (x_right_remaining / math.cos(math.radians(abs(turtle_angle - 360)))) - 0.01
+            if distance < 50:
+                return distance
+            else:
+                return 50
+        elif x_right_remaining >= 50 and y_down_remaining < 50:
+            distance = (y_down_remaining / math.sin(math.radians(abs(turtle_angle - 360)))) - 0.01
+            if distance < 50:
+                return distance
+            else:
+                return 50
+        elif x_right_remaining < 50 and y_down_remaining < 50:
+            x_hypotenuse = x_right_remaining / math.cos(math.radians(abs(turtle_angle - 360)))
+            y_hypotenuse = y_down_remaining / math.sin(math.radians(abs(turtle_angle - 360)))
+            x_add = math.cos(math.radians(abs(turtle_angle - 360))) * y_hypotenuse
+            y_add = math.sin(math.radians(abs(turtle_angle - 360))) * x_hypotenuse
+            if turtle_x + x_add <= right_bound and turtle_y - y_add >= lower_bound:
+                if x_hypotenuse > 50:
+                    return 50
+                else:
+                    return x_hypotenuse - 0.01
+            elif turtle_x + x_add > right_bound:
+                x_diff = x_add - ((turtle_x + x_add) - right_bound)
+                hypotenuse = x_diff / math.cos(math.radians(abs(turtle_angle - 360)))
+                return hypotenuse - 0.01
+            elif turtle_y - y_add < lower_bound:
+                y_diff = y_add - (abs((turtle_y - y_add) - lower_bound))
+                hypotenuse = y_diff / math.sin(math.radians(abs(turtle_angle - 360)))
+                return hypotenuse - 0.01
+        else:
+            return 50
 
+
+def find_distance(turtle, screen, quadrant, heading):
+    left_bound = -screen.window_width() / 2
+    right_bound = screen.window_width() / 2
+    upper_bound = screen.window_height() / 2
+    lower_bound = -screen.window_height() / 2
+
+    turtle_x = turtle.xcor()
+    turtle_y = turtle.ycor()
+    turtle_angle = turtle.heading()
+
+    x_left_remaining = abs(left_bound - turtle_x)
+    x_right_remaining = abs(right_bound - turtle_x)
+    y_up_remaining = abs(upper_bound - turtle_y)
+    y_down_remaining = abs(lower_bound - turtle_y)
+
+    if quadrant == "q1":
+        if heading == "q1":
+            return (corner_distance(turtle, screen, quadrant, heading))
+        elif heading == "q2":
+            if y_up_remaining >= 50:
+                return 50
+            else:
+                distance = (y_up_remaining / math.sin(math.radians(abs(turtle_angle - 180)))) - 0.01
+                if distance < 50:
+                    return distance
+                else:
+                    return 50
+        elif heading == "q3":
+            return 50
+        else:
+            if x_right_remaining >= 50:
+                return 50
+            else:
+                distance = (x_right_remaining / math.cos(math.radians(abs(turtle_angle - 360)))) - 0.01
+                if distance < 50:
+                    return distance
+                else:
+                    return 50
+    elif quadrant == "q2":
+        if heading == "q1":
+            if y_up_remaining >= 50:
+                return 50
+            else:
+                distance = (y_up_remaining / math.sin(math.radians(turtle_angle))) - 0.01
+                if distance < 50:
+                    return distance
+                else:
+                    return 50
+        elif heading == "q2":
+            return (corner_distance(turtle, screen, quadrant, heading))
+        elif heading == "q3":
+            if x_left_remaining >= 50:
+                return 50
+            else:
+                distance = (x_left_remaining / math.cos(math.radians(turtle_angle - 180))) - 0.01
+                if distance < 50:
+                    return distance
+                else:
+                    return 50
+        else:
+            return 50
+    elif quadrant == "q3":
+        if heading == "q1":
+            return 50
+        elif heading == "q2":
+            if x_left_remaining >= 50:
+                return 50
+            else:
+                distance = (x_left_remaining / math.cos(math.radians(abs(turtle_angle - 180)))) - 0.01
+                if distance < 50:
+                    return distance
+                else:
+                    return 50
+        elif heading == "q3":
+            return (corner_distance(turtle, screen, quadrant, heading))
+        else:
+            if y_down_remaining >= 50:
+                return 50
+            else:
+                distance = (y_down_remaining / math.sin(math.radians(abs(turtle_angle - 360)))) - 0.01
+                if distance < 50:
+                    return distance
+                else:
+                    return 50
+    else:
+        if heading == "q1":
+            if x_right_remaining >= 50:
+                return 50
+            else:
+                distance = (x_right_remaining / math.cos(math.radians(turtle_angle))) - 0.01
+                if distance < 50:
+                    return distance
+                else:
+                    return 50
+        elif heading == "q2":
+            return 50
+        elif heading == "q3":
+            if y_down_remaining >= 50:
+                return 50
+            else:
+                distance = (y_down_remaining / math.sin(math.radians(turtle_angle - 180))) - 0.01
+                if distance < 50:
+                    return distance
+                else:
+                    return 50
+        else:
+            return (corner_distance(turtle, screen, quadrant, heading))
+
+
+def move_distance(screen, turtle):
     # which quadrant is the turtle in?
     quadrant = which_quadrant(turtle)
 
     # which quadrant is it facing?
     angle_direction = which_direction(turtle)
 
-    # if x is closet
-    # once you have those two you know the math you need to do
-    # corners will be the most concern
-    # which corner closer? function
-    turtle.forward(find_distance(turtle, quadrant, angle_direction)
-
-    """
-    if turtle_x >= 0 and turtle_y >=0:
-        if 180 < turtle_angle < 270:
-            turtle.forward(50)
-        else:
-            if 90 < turtle_angle < 180:
-                if y_up_remaining > 50:
-                    turtle.forward(50)
-                else:
-    """ \
-    """
-    if x_left_remaining < 50:
-        if 90 < turtle_angle < 270:
-            if turtle_y > 0:
-                if y_up_remaining < 50 and y_up_remaining - opposite > 0:
-                    print()
-                """
+    return find_distance(turtle, screen, quadrant, angle_direction)
 
 
 def is_in_screen(screen, turtle):
@@ -101,14 +313,18 @@ def is_in_screen(screen, turtle):
     return still_in
 
 
-def random_move(turtle):
+def random_move(screen, turtle):
     coin = random.randrange(0, 2)
     if coin == 0:
         turtle.left(random.randrange(0, 180))
     else:
         turtle.right(random.randrange(0, 180))
 
-    turtle.forward(50)
+    distance = move_distance(screen, turtle)
+    turtle.forward(distance)
+    if distance < 50:
+        turtle.right(180)
+        turtle.forward(50 - distance)
 
 
 def main():
@@ -117,7 +333,9 @@ def main():
     wn = turtle.Screen()
 
     mildred.shape("turtle")
+    mildred.color("red")
     jeb.shape("turtle")
+    jeb.color("blue")
 
     mildred.hideturtle()
     mildred.up()
@@ -137,8 +355,8 @@ def main():
     while is_in_screen(wn, mildred):
         if not is_in_screen(wn, jeb):
             break
-        random_move(mildred)
-        random_move(jeb)
+        random_move(wn, mildred)
+        random_move(wn, jeb)
 
     wn.exitonclick()
 
