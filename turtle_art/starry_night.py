9d7af8f1b82b2795ee290b6522bd081d2db05f68
Timothy Landwehr <tjlandwehr@gmail.com>
2017-11-12 14:10:52 -0600

    Added a draw_planet() function

--- turtle_art/starry_night.py
+++ turtle_art/starry_night.py
@@ -1,15 +1,41 @@
 import turtle
+from random import randint, random
 
-def main():
-    t = turtle.Turtle()
+def draw_planet(col, x, y):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    t.color(col)
+    t.begin_fill()
+    t.circle(50)
+    t.end_fill()
 
-    size = 300
-    points = 5
-    angle = 144
-
+def draw_star(t, points, size, col, x, y):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    angle = 180 - (180 / points)
+    t.color(col)
+    t.begin_fill()
     for i in range(points):
         t.forward(size)
         t.right(angle)
+    t.end_fill()
+
+def main():
+    t = turtle.Turtle()
+    wn = turtle.Screen()
+    t.speed(0)
+    t.hideturtle()
+    wn.bgcolor('dark blue')
+    while True:
+        ranPts = randint(2, 5) * 2 + 1
+        ranSize = randint(10, 50)
+        ranCol = (random(), random(), random())
+        ranX = randint(-350, 300)
+        ranY = randint(-250, 250)
+
+        draw_star(t, ranPts, ranSize, ranCol, ranX, ranY)
 
 if __name__ == "__main__":
     main()
