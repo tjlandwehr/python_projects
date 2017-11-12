062b660d853d946dd2c1d40a2f8453f0ab3b33d7
Timothy Landwehr <tjlandwehr@gmail.com>
2017-11-12 13:04:14 -0600

    Changed draw_circle() fuction to draw_shape() function which alternates between circles and squares.

--- turtle_art/kaleido_spiral.py
+++ turtle_art/kaleido_spiral.py
@@ -3,12 +3,28 @@ from itertools import cycle
 
 colors = cycle(['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
 
-def draw_circle(size, angle, shift):
+# def draw_circle(size, angle, shift):
+#     turtle.bgcolor(next(colors))
+#     turtle.pencolor(next(colors))
+#     turtle.circle(size)
+#     turtle.right(angle)
+#     turtle.forward(shift)
+#     draw_circle(size + 5, angle + 1, shift + 1)
+
+def draw_shape(size, angle, shift, shape):
     turtle.pencolor(next(colors))
-    turtle.circle(size)
+    next_shape = ''
+    if shape == 'circle':
+        turtle.circle(size)
+        next_shape = 'square'
+    elif shape == 'square':
+        for i in range(4):
+            turtle.forward(size * 2)
+            turtle.left(90)
+        next_shape = 'circle'
     turtle.right(angle)
     turtle.forward(shift)
-    draw_circle(size + 5, angle + 1, shift + 1)
+    draw_shape(size + 5, angle + 1, shift + 1, next_shape)
 
 def main():
     turtle.bgcolor('black')
@@ -16,7 +32,7 @@ def main():
     turtle.pensize(4)
 
     turtle.pencolor('red')
-    draw_circle(30, 0, 1)
+    draw_shape(30, 0, 1, 'circle')
 
 if __name__ == "__main__":
     main()
